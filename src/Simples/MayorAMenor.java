32478b6ec4146f4fbd670a4f90d54b55e88cd46a
ablanco <alvaroblanconsangines.to@gmail.com>
2018-10-30 12:48:44 +0100

    Mayor a menor añadido

--- src/Simples/MayorAMenor.java
+++ src/Simples/MayorAMenor.java
@@ -34,6 +34,38 @@ public class MayorAMenor {
 		return menor;
 	}
 
+	private int diferencia(int[] arraySuma) {
+		int dif = 0;
+		for (int i = 1; i < arraySuma.length; i++) {
+			// Comparar un número arraySuma[i] con el siguiente arraySuma[i+1]
+			dif = dif + Math.abs(arraySuma[i - 1] - arraySuma[i]);
+		}
+
+		return dif;
+	}
+
+	private int diferencia2(int[] arraySuma) {
+		int dif = 0;
+		for (int i = arraySuma.length - 1; i > 0; i--) {
+			// Comparar un número arraySuma[i] con el siguiente arraySuma[i+1]
+			dif = dif + Math.abs(arraySuma[i - 1] - arraySuma[i]);
+		}
+
+		return dif;
+	}
+
+	private boolean orden(int[] arraySuma) {
+		boolean res = false;
+		String respuesta = "";
+		for (int i = 0; i < arraySuma.length - 1; i++) {
+			if (arraySuma[i] > arraySuma[i + 1]) {
+				res = false;
+			}
+		}
+		return res;
+
+	}
+
 	public static void main(String[] args) {
 
 		int[] arraySuma = { 2, 5, 1, 3, 4 };
@@ -42,16 +74,22 @@ public class MayorAMenor {
 		int mayor = 0;
 		int menor = 0;
 		int rango = 0;
-
+		int diferencia = 0;
+		boolean orden;
 		// proceso y salida
 		programa.imprimir(arraySuma);
 		suma = programa.sumaTotal(arraySuma);
 		mayor = programa.mayor(arraySuma);
 		menor = programa.menor(arraySuma);
+		diferencia = programa.diferencia(arraySuma);
+		orden = programa.orden(arraySuma);
 		rango = mayor - menor;
+
 		System.out.println("La suma de los números es " + " " + suma);
 		System.out.println("El mayor de los números es " + " " + mayor);
 		System.out.println("El rango es " + " " + rango);
+		System.out.println("La suma de la diferencia es  " + " " + diferencia);
+		System.out.println("El número del orden es" + " " + orden);
 	}
 
 }
