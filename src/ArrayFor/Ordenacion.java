59c1fcdab5741e58e7c968f42cf15b3bcf9159cd
Álvaro Blanco Sanginés <35927676+ablanco2@users.noreply.github.com>
2019-12-02 18:23:31 +0100

    Ordenar los numeros de menor a mayor
    Pide por teclado para que el usuario introduzca los cinco numeros y a la entrada ordenará de menor a mayor

---
+++ src/ArrayFor/Ordenacion.java
@@ -0,0 +1,23 @@
+public class Ordenacion {
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		int[] vector = new int[5];
+		System.out.println("Escribe los cinco numeros enteros: ");
+		for (int i = 0; i < vector.length; i++) {
+			vector[i] = in.nextInt();
+		}
+		int aux;
+		for (int k = 0; k < vector.length; k++) {
+			for (int i = 0; i < vector.length - k - 1; i++) {
+				if (vector[i] > vector[i + 1]) {
+					aux = vector[i];
+					vector[i] = vector[i + 1];
+					vector[i + 1] = aux;
+				}
+			}
+		}
+		System.out.println(Arrays.toString(vector));
+	}
+
+}
